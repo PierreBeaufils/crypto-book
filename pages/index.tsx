45d6227d73a5f894dcbeb9b62dab7b8b7dd4f233
Pierre Laptop <pierre.beaufils@outlook.fr>
2021-03-18 18:58:12 +0100

    setup tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,8 @@ import Link from 'next/link'
 import Layout from '../components/Layout'
 
 const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
+  <Layout title="Crypto Book">
+    <h1 className="glass">Hello Next.js </h1>
     <p>
       <Link href="/about">
         <a>About</a>
