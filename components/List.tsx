56f893abc9911d902910a816763d01e64a76a4a7
Pierre Laptop <pierre.beaufils@outlook.fr>
2021-03-19 18:44:30 +0100

    table data

--- components/List.tsx
+++ components/List.tsx
@@ -1,20 +1,111 @@
 import * as React from 'react'
 import Image from 'next/image'
+import { useTable, useSortBy, useFilters } from 'react-table';
 import ListItem from './ListItem'
 import { Crypto } from '../interfaces'
+import styles from '../styles/list.module.scss'
 
 type Props = {
-  items: Crypto[]
+  data: Crypto[]
 }
 
-const List = ({ items }: Props) => (
-  <ul>
-    {items.map((item) => (
-      <li key={item.id}>
-        <ListItem data={item} />
-      </li>
-    ))}
-  </ul>
-)
+const List = ({ data }: Props) => {
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'Rank',
+        accessor: 'rank', // accessor is the "key" in the data
+      },
+      {
+        Header: 'Name',
+        Cell: ({ row }) => (
+          <div className={styles.name}>
+            <span>
+              <Image
+                src={row.original.logo_url}
+                alt="logo"
+                width={20}
+                height={20}
+                onClick={()=>console.log(row)}
+                />
+              </span>
+            <div>
+              <h4>{row.original.name}</h4>
+              <p>{row.original.currency}</p>
+            </div>
+          </div>
+        ),
+      },
+      {
+        Header: 'Price',
+        accessor: 'price',
+      },
+      {
+        Header: 'Market Cap',
+        accessor: 'market_cap',
+      },
+      {
+        Header: 'Volume (24h)',
+        accessor: '1d.volume',
+      },
+      {
+        Header: 'Circulating Supply',
+        accessor: 'circulating_supply',
+      },
+    ],
+    [],
+  );
+
+  const {
+    getTableProps, // table props from react-table
+    getTableBodyProps, // table body props from react-table
+    headerGroups, // headerGroups, if your table has groupings
+    rows, // rows for the table based on the data passed
+    prepareRow, // this function needs to be called for each row before getting the row props
+    setFilter,
+  } = useTable({
+    columns,
+    data,
+  }, useFilters, useSortBy);
+
+  return (
+    <table {...getTableProps()} className={styles.cryptoList}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th
+                  {...column.getHeaderProps(column.getSortByToggleProps())}
+                  className={styles.tableHeader}
+                >
+                  {column.render('Header')}
+                  <span>
+                    {column.isSorted ? (column.isSortedDesc ? ' 🔽' : ' 🔼') : ''}
+                  </span>
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row) => {
+            prepareRow(row);
+            return (
+              <tr className={styles.cryptoItem} {...row.getRowProps()} >
+                {row.cells.map((cell) => (
+                  <td
+                    {...cell.getCellProps()}
+                    className={styles.cryptoData}
+                  >
+                    {cell.render('Cell')}
+                  </td>
+                ))}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+  )
+}
 
 export default List
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
 import Link from 'next/link'
 import { GetStaticProps, GetStaticPaths, GetServerSideProps } from 'next'
 import Layout from '../components/Layout'
+import List from '../components/List'
 
-const IndexPage = ({res}) => (
+const IndexPage = ({data}: any) => (
   <Layout title="Crypto Book">
     <h1 className="glass">Hello Next.js </h1>
     <p>
@@ -10,24 +11,16 @@ const IndexPage = ({res}) => (
         <a>About</a>
       </Link>
     </p>
-    <ul>
-      {res.map((item) => (
-        <li key={item.id}>
-          {item.name}
-        </li>
-      ))}
-    </ul>
+    <List data={data} />
   </Layout>
 )
 
 export const getStaticProps: GetStaticProps = async () => {
-  // Example for including static props in a Next.js function component page.
-  // Don't forget to include the respective types for any props passed into
-  // the component.
-  const res = await fetch(`https://api.nomics.com/v1/currencies/ticker?key=${process.env.API_KEY}&interval=1d,30d&convert=EUR&per-page=100&page=1`)
+
+  const data = await fetch(`https://api.nomics.com/v1/currencies/ticker?key=${process.env.API_KEY}&interval=1d,30d&convert=EUR&per-page=100&page=1`)
     .then(res => res.json())
   
-  return { props: { res } }
+  return { props: { data } }
 }
 
 export default IndexPage
